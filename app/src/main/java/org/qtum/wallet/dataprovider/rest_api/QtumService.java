b33dd81818e5f785ed7afaec652d41e75bc1bfcf
Vu Do <dohaivu@gmail.com>
2018-03-20 10:37:23 +0700

    #56 comment old code, add another pin

--- app/src/main/java/org/qtum/wallet/dataprovider/rest_api/QtumService.java
+++ app/src/main/java/org/qtum/wallet/dataprovider/rest_api/QtumService.java
@@ -71,23 +71,23 @@ public class QtumService {
 
     private QtumService() {
         try {
-            final TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        @Override
-                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType){
-                        }
-
-                        @Override
-                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType){
-
-                        }
-
-                        @Override
-                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                            return new java.security.cert.X509Certificate[]{};
-                        }
-                    }
-            };
+//            final TrustManager[] trustAllCerts = new TrustManager[]{
+//                    new X509TrustManager() {
+//                        @Override
+//                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType){
+//                        }
+//
+//                        @Override
+//                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType){
+//
+//                        }
+//
+//                        @Override
+//                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+//                            return new java.security.cert.X509Certificate[]{};
+//                        }
+//                    }
+//            };
 
             // Install the all-trusting trust manager
 
@@ -114,16 +114,16 @@ public class QtumService {
 //                }
 //            });
 
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keyStore.load(null, null);
-
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init(keyStore);
-            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            keyManagerFactory.init(keyStore, "keystore_pass".toCharArray());
-            sslContext.init(null, trustAllCerts, new SecureRandom());
+//            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+//            keyStore.load(null, null);
+//
+//            SSLContext sslContext = SSLContext.getInstance("TLS");
+//
+//            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+//            trustManagerFactory.init(keyStore);
+//            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+//            keyManagerFactory.init(keyStore, "keystore_pass".toCharArray());
+//            sslContext.init(null, trustAllCerts, new SecureRandom());
 //            client.sslSocketFactory(sslContext.getSocketFactory());
 //            client.hostnameVerifier(new HostnameVerifier() {
 //                        @Override
